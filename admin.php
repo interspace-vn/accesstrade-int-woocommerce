15789798ab591203a76f4d7f183fc58ac97a1785
thanhnguyenduy997 <45163396+thanhnguyenduy997@users.noreply.github.com>
2019-04-17 14:08:46 +0700

    Add files via upload

--- admin.php
+++ admin.php
@@ -21,7 +21,13 @@ function at_int_woocommerce_options(){
             <form class="w3-container" action="admin.php?page=plugin-options" method="post">
                 <h1>------------ Token ------------</h1>                
                 <label>Token: </label>
-                <input class="w3-input" type="text" name="token">
+                <input class="w3-input" type="text" name="token">                
+                <label>Campaign_id: </label>
+                <input class="w3-input" type="text" name="campaign_id">                
+                <label>Is_reoccur: </label>
+                <input class="w3-input" type="text" name="is_reoccur">                
+                <label>Is_lastclick: </label>
+                <input class="w3-input" type="text" name="is_lastclick">
                 <hr>
                 <input type="submit" name="submit" value="submit" />             
             </form>
@@ -30,8 +36,14 @@ function at_int_woocommerce_options(){
     <?php
 
     $token = $_POST['token'];
+    $campaign_id = $_POST['campaign_id'];
+    $is_reoccur = $_POST['is_reoccur'];
+    $is_lastclick = $_POST['is_lastclick'];
 
-    if (isset($token)) {
+    if (isset($token) && $token != '' && isset($campaign_id) && $campaign_id != '' && isset($is_reoccur) && $is_reoccur != '' && isset($is_lastclick) && $is_lastclick != '') {
         update_option( "token", $token );
+        update_option( "campaign_id", $campaign_id );
+        update_option( "is_reoccur", $is_reoccur );
+        update_option( "is_lastclick", $is_lastclick );
     }
 }
