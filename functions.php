f76f86dafde29791edb70870b2e00663d402fdfa
thanhnguyenduy997 <45163396+thanhnguyenduy997@users.noreply.github.com>
2019-04-17 14:03:50 +0700

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,140 +1,133 @@
 <?php 
-add_action('woocommerce_checkout_order_processed', 'enroll_student');
+add_action('woocommerce_checkout_order_processed', 'request_api_accesstrade');
 
-    function enroll_student($order_id)
-    {
-        $order = wc_get_order( $order_id );
+function request_api_accesstrade($order_id)
+{
+    $order = wc_get_order( $order_id );
 
-        $line_items = $order->get_items();
+    $line_items = $order->get_items();
 
-        $items=array();
+    $items=array();
 
-        $date = date('Y-m-d H:i:s');
+    $date = date('Y-m-d H:i:s');
 
-        foreach ( $line_items as $item ) {
+    foreach ( $line_items as $item ) {
             // This will be a product
-            $product = $order->get_product_from_item( $item );
+        $product = $order->get_product_from_item( $item );
 
             // This is the products SKU
-            $sku = $product->get_sku();
+        $sku = $product->get_sku();
 
             // This is the qty purchased
-            $qty = $item['qty'];
+        $qty = $item['qty'];
 
             // Line item total cost including taxes and rounded
-            $total = $order->get_line_total( $item, true, true );
+        $total = $order->get_line_total( $item, true, true );
 
             // Line item subtotal (before discounts)
-            $subtotal = $order->get_line_subtotal( $item, true, true );
+        $subtotal = $order->get_line_subtotal( $item, true, true );
 
-            array_push($items, $item);
+        array_push($items, $item);
 
-        }
-        $_SESSION["items"] = $items;
-        $arr = array();
-        for ($i=0; $i < sizeof($_SESSION["items"]); $i++) { 
-            array_push($arr, array(
-                        "id" => strval($items[$i]['product_id']),
-                        "sku" => " ",
-                        "name" => strval($items[$i]['name']),
-                        "price" => $items[$i]['subtotal'],
-                        "quantity" => $items[$i]['quantity'],
-                        "category" => "Category A",
-                        "category_id" => "defaul"
-                    ));     
-        }
-        $data1 = array(
-            "conversion_id" => strval($order_id),
-            "conversion_result_id" => "30",
-            "tracking_id" => "xxx",
-            "transaction_id" => strval($order_id),
-            "transaction_time" => $date,
-            "transaction_value" => 100000,
-            "transaction_discount " => strval($order->discount_total),
-            "items" => $arr
-        );
-
-        $data_string = json_encode($data1);
-
-        // echo $data_string;
-
-
-        $ch = curl_init('https://api.accesstrade.vn/v1/postbacks/conversions');
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Authorization: Token '.get_option( "token", $default = false)
-        ));
-
-        $result = curl_exec($ch);
-
-        echo $order_id;
     }
+    $_SESSION["items"] = $items;
+    $arr_item = array();
+    $count = sizeof($_SESSION["items"]);
+    echo $count;
+    for ($i=0; $i < $count; $i++) { 
+        array_push($arr_item, array(
+            "id" => strval($items[$i]['product_id']),
+            "sku" => strval($items[$i]['product_id']),
+            "name" => strval($items[$i]['name']),
+            "price" => $items[$i]['subtotal'],
+            "quantity" => $items[$i]['quantity'],
+            "category" => "Category A",
+            "category_id" => "defaul"
+        ));     
+    }
+    $data1 = array(
+        "conversion_id" => strval($order_id),
+        "conversion_result_id" => "30",
+        "tracking_id" => "xxx",
+        "transaction_id" => strval($order_id),
+        "transaction_time" => $date,
+        "transaction_value" => 100000,
+        "transaction_discount " => strval($order->discount_total),
+        "items" => $arr_item
+    );
 
-    // function bbloomer_conversion_tracking_thank_you_page() {
-    //  // print_r($_SESSION["items"]);
-    
-    // }
+    $data_string = json_encode($data1);
 
-    function wpb_hook_javascript() {
+    $url='https://api.accesstrade.vn/v1/postbacks/conversions';
+
+    $response = wp_remote_post( $url, array(
+        'method' => 'POST',
+        'body'    => $data_string,
+        'headers' => array(
+            'Content-Type'=>' application/json',
+            'Authorization'=>'Token '.get_option( "token", $default = false)
+        ),
+    ) );
+
+}
+
+
+
+function wpb_hook_javascript() {
 ?>
-        <script src="//cdn.accesstrade.vn/js/tracking.js" ></script>
-        <script type="text/javascript">
-                AT.track();
-        </script>
+    <script src="//static.accesstrade.vn/js/trackingtag/tracking.min.js " ></script> 
+    <script type="text/javascript">
+        AT.init({"campaign_id":<?php echo get_option( "campaign_id", $default = false) ?>, "is_reoccur": <?php echo get_option( "is_reoccur", $default = false) ?>,"is_lastclick": <?php echo get_option( "is_lastclick", $default = false) ?>} );
+        AT.track();
+    </script>
+
 <?php
+}
+add_action('wp_head', 'wpb_hook_javascript');
+
+add_action('woocommerce_order_status_changed','status_api_accesstrade');
+function status_api_accesstrade( $order_id, $checkout = null ) {
+ $order = wc_get_order( $order_id );
+ $line_items = $order->get_items();
+
+ $items=array();
+
+ foreach ( $line_items as $item ) {
+
+    array_push($items, $item);
+
+}
+
+if($order->status == "completed"){
+
+    $arr_item = array();
+    $count = sizeof($items);
+    for ($i=0; $i < $count; $i++) { 
+        array_push($arr_item, array(
+            "id" => strval($items[$i]['product_id']),
+            "status" => 1
+        ));     
     }
-    add_action('wp_head', 'wpb_hook_javascript');
+    $data1 = array(
+        "transaction_id" => strval($order_id),
+        "status" => 1,
+        "items" => $arr_item
+    );
 
-    add_action('woocommerce_order_status_changed','status_changed_processsing');
-   function status_changed_processsing( $order_id, $checkout = null ) {
-       $order = wc_get_order( $order_id );
-       $line_items = $order->get_items();
-       echo $order->status;
+    $data_string = json_encode($data1);
 
-       $items=array();
+    $url='https://api.accesstrade.vn/v1/postbacks/conversions';
 
-        foreach ( $line_items as $item ) {
+    $response = wp_remote_post( $url, array(
+        'method' => 'PUT',
+        'body'=> $data_string,
+        'headers' => array(
+        'Content-Type'=>' application/json',
+        'Authorization'=>'Token '.get_option( "token", $default = false)
+        )
+    ) );
+    
 
-            array_push($items, $item);
-
-        }
-
-       if($order->status == "completed"){
-
-            $arr = array();
-            for ($i=0; $i < sizeof($items); $i++) { 
-                array_push($arr, array(
-                            "id" => strval($items[$i]['product_id']),
-                            "status" => 1
-                        ));     
-            }
-            $data1 = array(
-                "transaction_id" => strval($order_id),
-                "status" => 1,
-                "items" => $arr
-            );
-
-            $data_string = json_encode($data1);
-            echo $data_string;
-
-
-            $ch = curl_init('https://api.accesstrade.vn/v1/postbacks/conversions');
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                'Authorization: Token '.get_option( "token", $default = false)
-            ));
-
-            $result = curl_exec($ch);
-
-            echo $result;
-
-           
-        }
-   }
- ?>
+}
+}
+?>
